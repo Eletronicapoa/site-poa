ddad28908ff4de8fcadb69942b48102d2250fde3
Eletronicapoa <eletronicalabpoa@gmail.com>
2025-08-26 14:11:23 -0300

    first commit

---
+++ POA site v2/scripts/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const timeline = document.querySelector('.timeline-horizontal');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  if (timeline) {
+    timeline.addEventListener('mousedown', (e) => {
+      isDown = true;
+      timeline.classList.add('active');
+      startX = e.pageX - timeline.offsetLeft;
+      scrollLeft = timeline.scrollLeft;
+      e.preventDefault();
+    });
+
+    timeline.addEventListener('mouseleave', () => {
+      isDown = false;
+      timeline.classList.remove('active');
+    });
+
+    timeline.addEventListener('mouseup', () => {
+      isDown = false;
+      timeline.classList.remove('active');
+    });
+
+    timeline.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - timeline.offsetLeft;
+      const walk = (x - startX) * 2; // multiplica para aumentar a velocidade
+      timeline.scrollLeft = scrollLeft - walk;
+    });
+
+    // Mantém o scroll com a roda do mouse
+    timeline.addEventListener('wheel', function (e) {
+      if (e.deltaY !== 0) {
+        e.preventDefault();
+        timeline.scrollLeft += e.deltaY * 3;
+      }
+    });
+  }
+});
