df2d9f4288c47f2592f13ab347165f0f0eff063a
Eletronicapoa <eletronicalabpoa@gmail.com>
2025-11-19 18:20:41 -0300

    removendo blocos de código desnecessários

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,61 @@
-document.addEventListener("DOMContentLoaded", function () {
+/* JS dos slides */
+const swiperSlides = new Swiper(".swiper-slides", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+/* js da seçao publicações (livros) */
+var swiperLivros = new Swiper(".swiper-livros", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 2,
+    stretch: 20,
+    depth: 80,
+    modifier: 7.2,
+    slideShadows: true,
+  },
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  loopedSlides: 4,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  keyboard: {
+    enable: true,
+  },
+  mousewheel: {
+    thresholdDelta: 990,
+  },
+  /* breakpoints:{
+          560:{
+            slidesPerView: 2
+          },
+          768:{
+            slidesPerView: 2
+          },
+          1024: {
+            slidesPerView: 3
+          }
+        }  */
+});
+
+/* !!!!!Script que não serve pra nada (oq foi que a julia fez aqui????????)!!!!!!!!!!!!!! */
+
+/* document.addEventListener("DOMContentLoaded", function () {
   const timeline = document.querySelector('.timeline-horizontal');
   let isDown = false;
   let startX;
@@ -48,8 +105,14 @@ setInterval( function(){
   nextImage();
 }, 2000)
 
+/* const swiper = new Swiper('.swiper',
+  {
+    slidesPerView: 3
 
-function nextImage(){
+  }
+) */
+
+/* function nextImage(){
   count++;
   if(count>4){
     count = 1;
@@ -58,3 +121,4 @@ function nextImage(){
   document.getElementById('radio'+count).checked = true;
 
 };
+ */
